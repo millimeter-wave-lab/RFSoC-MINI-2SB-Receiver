5aa0c04602c35294945cd4b16b6bd793ff6c2993
jvargas2001 <josevargas@ug.uchile.cl>
2025-06-12 14:57:27 -0400

    Script documentation updated.

--- test_spec_cnt.py
+++ test_spec_cnt.py
@@ -5,21 +5,19 @@ import time
 This signal prepares the spectrometer for every cycle of integration and resets
 the counter of accumulation in every falling edge.'''
 
-fpga = casperfpga.CasperFpga('10.17.90.40')     # Use your RFSoC IP address
+fpga = casperfpga.CasperFpga('10.17.90.187')     # Use your RFSoC IP address
 print('Done')
 
-fpga.upload_to_ram_and_program('8192ch_32bits_reset/dss_ideal_8192ch_32bits_reset_1966mhz_cx.fpg')
+fpga.upload_to_ram_and_program('/home/jose/Workspace/RFSoC-MINI-2SB-Receiver/32_bits_models/8192ch_32bits_reset/dss_ideal_first_quant_8192ch_32bits_reset_1966mhz_cx.fpg')
 
 print('Configuring accumulation period...')
 fpga.write_int('acc_len', 2**13)
-# fpga.write_int('acc_len_pic', 2**12)
+# fpga.write_int('acc_len_re_bin', 2**12)
 time.sleep(1)
 print('Done')
 
 print('Resetting counters...')
-time.sleep(1)
 fpga.write_int('cnt_rst', 1)
-time.sleep(1)
 fpga.write_int('cnt_rst', 0)
 time.sleep(1)
 print('Done')
